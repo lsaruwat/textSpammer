0b04fb698efcb3fbb40f89625dddc3db7fc86a1c
Logan Saruwatari <lsaruwatari@gmail.com>
2018-03-16 23:29:44 -0600

    changed to 10 digit number and only using sms email

--- mailer.php
+++ mailer.php
@@ -6,6 +6,6 @@ $msg = "First line of text\nSecond line of text";
 $msg = wordwrap($msg,70);
 
 // send email
-var_dump( mail("19702703575@mms.att.net","My subject",$msg) );
-var_dump( mail("19702703575@txt.att.net","My subject",$msg) );
+//var_dump( mail("19702703575@mms.att.net","My subject",$msg) );
+var_dump( mail("9702703575@txt.att.net","My subject",$msg) );
 ?>
